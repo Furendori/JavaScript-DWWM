bea39cbc8d035bfacf56520dd2254a07e5e9e20e
Julian Brasseur <brasseurjulian2@gmail.com>
2023-07-06 18:48:44 +0200

    Suite exos JS

--- exo6.js
+++ exo6.js
@@ -1,31 +1,29 @@
-let newUl = document.createElement("ul")
-newUl.setAttribute("class", "newUl")
+let newUl = document.createElement("ul");
+newUl.setAttribute("class", "newUl");
 
-let li1 = document.createElement('li')
-li1.textContent = 'Faire les courses'
+let li1 = document.createElement("li");
+li1.textContent = "Faire les courses";
 
-let li2 = document.createElement('li')
-li2.textContent = 'Regarder des films'
+let li2 = document.createElement("li");
+li2.textContent = "Regarder des films";
 
-let li3 = document.createElement('li')
-li3.textContent = 'Regarder des films'
+let li3 = document.createElement("li");
+li3.textContent = "Regarder des films";
 
-let li4 = document.createElement('li')
-li4.textContent = 'Regarder des films'
+let li4 = document.createElement("li");
+li4.textContent = "Regarder des films";
 
-let newLi = document.getElementsByTagName('li')
+let newLi = document.getElementsByTagName("li");
 let ligneSuppr;
 
-for (let i = 0; i < newLi.length; i++) {
-  for (let j = 0; j < newLi.length; j++) {
-    newLi[i].addEventListener("click", function () {
-      
-    });
-  }
-}
+document.body.appendChild(newUl);
+newUl.appendChild(li1);
+newUl.appendChild(li2);
+newUl.appendChild(li3);
+newUl.appendChild(li4);
 
-document.body.appendChild(newUl)
-newUl.appendChild(li1)
-newUl.appendChild(li2)
-newUl.appendChild(li3)
-newUl.appendChild(li4)
+for (let i = 0; i < newLi.length; i++) {
+  newLi[i].addEventListener("click", function () {
+    this.remove();
+  });
+}
